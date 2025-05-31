d70ca121d32a20e88ca5cec4bd364ca25d5aed48
Alan W <awangc31s@gmail.com>
2025-05-31 17:32:03 -0400

    pixi

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/zsh
-echo "This compile file uses the uv tool. You can find and install uv here:
-https://github.com/astral-sh/uv" 
-
-uv venv server-venv
-source server-venv/bin/activate
-uv pip install -r requirements.txt
