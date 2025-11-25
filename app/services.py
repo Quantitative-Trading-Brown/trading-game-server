4f26c61fd65b49d833f0431b4cd47f1f178d122b
Alan W <awangc31s@gmail.com>
2025-11-25 00:04:01 -0500

    Stuff

--- app/services.py
+++ app/services.py
@@ -5,5 +5,6 @@ socketio = SocketIO(
     cors_allowed_origins=["https://simulator.qtab.site", "http://localhost:3000"],
     async_mode="threading",
 )
-r = valkey.Valkey(host="localhost", port=6379, db=0, decode_responses=True)
 sid = lambda x : getattr(x, "sid", None)
+
+r = valkey.Valkey(host="localhost", port=6379, db=0, decode_responses=True)
