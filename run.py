0f01b9ce89f6a8e717d9d118a4c09169d994921d
Alan W <awangc31s@gmail.com>
2026-01-09 16:04:49 -0500

    Stuff

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app import create_app, socketio
-import os
-
 
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host=app.config["HOST"], port=app.config["PORT"])
